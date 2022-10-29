8b71c4ce6d3be88643a41ae75bbda21e21894a75
Lucas Freitas <lucas@lucasfreitas.com.br>
2022-10-29 15:29:51 +0200

    Changes the logic of OU enumeration from indexed array to map.

--- organization.tf
+++ organization.tf
@@ -1,4 +1,5 @@
 resource "aws_organizations_organization" "organization" {
   feature_set = local.feature_set
   aws_service_access_principals = local.aws_service_access_principals
+  enabled_policy_types = var.enabled_policy_types
 }
